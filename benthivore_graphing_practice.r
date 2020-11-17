a9ccf96f085962d88d082d6c8db5801ede0e643c
Ricky Tabandera <rickytabandera@gmail.com>
2020-11-17 10:49:37 -1000

    after git update

--- benthivore_graphing_practice.r
+++ benthivore_graphing_practice.r
@@ -1,5 +1,6 @@
 remotes::install_github("noaa-edab/ecodata",build_vignettes=TRUE)
 library(tidyverse)
+library(remotes)
 # Loading the bottom traw data from NEFCS simplified into guilds
 trawl_survey<-ecodata::nefsc_survey
 # Selecting out only the fall benthivore data 
@@ -13,3 +14,6 @@ ggplot(benthivore_fall, aes(x=Time, y= Value))+
   geom_point()
 stock_list_all_strata <- read.csv("https://raw.githubusercontent.com/NOAA-EDAB/ECSA/master/data/stock_list.csv")
 stock_list<-unique(stock_list_all_strata$sci_name)
+remotes::install_github("https://github.com/NOAA-EDAB/assessmentdata.git")
+
+g
